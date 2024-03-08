23a26251b902105f94e0a769b2e8d4515ea50d74
luka <lpavlovic032@gmail.com>
2024-03-08 16:53:30 +0100

    impelemted user repository

---
+++ IAMService/src/main/java/rs/edu/raf/IAMService/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package rs.edu.raf.IAMService.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rs.edu.raf.IAMService.data.entites.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+}
