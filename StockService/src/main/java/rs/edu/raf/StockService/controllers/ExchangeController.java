6d104e13b2ff3b56a932d8a287b444b8294fb0bb
SrdjanSimonovic9220 <ssimonovic9220rn@raf.rs>
2024-03-11 00:33:21 +0100

    Pushed Exchange controller and repository

---
+++ StockService/src/main/java/rs/edu/raf/StockService/controllers/ExchangeController.java
@@ -0,0 +1,42 @@
+package rs.edu.raf.StockService.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import rs.edu.raf.StockService.data.entities.Exchange;
+import rs.edu.raf.StockService.services.ExchangeService;
+
+import java.util.List;
+
+@RestController("/exchange")
+public class ExchangeController {
+
+    private final ExchangeService exchangeService;
+
+    public ExchangeController(ExchangeService exchangeService) {
+        this.exchangeService = exchangeService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Exchange>> findAllExchanges() {
+        return ResponseEntity.ok(exchangeService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Exchange> findExchangeById(@PathVariable Long id) {
+        return ResponseEntity.ok(exchangeService.findById(id));
+    }
+
+    @GetMapping("/exchangeName/{exchangeName}")
+    public ResponseEntity<Exchange> findExchangeByName(@PathVariable String exchangeName) {
+        return ResponseEntity.ok(exchangeService.findByExchangeName(exchangeName));
+    }
+
+    @GetMapping("/miCode/{miCode}")
+    public ResponseEntity<Exchange> findExchangeByMiCode(@PathVariable String miCode) {
+        return ResponseEntity.ok(exchangeService.findByMICode(miCode));
+    }
+
+
+}
