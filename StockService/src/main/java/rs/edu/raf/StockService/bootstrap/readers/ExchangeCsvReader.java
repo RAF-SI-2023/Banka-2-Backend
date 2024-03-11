17bc9b703e145818e949f903409b3dd51e7319d4
SrdjanSimonovic9220 <ssimonovic9220rn@raf.rs>
2024-03-11 18:35:34 +0100

    Finished initial test round of Exchange data from CSV file.

---
+++ StockService/src/main/java/rs/edu/raf/StockService/bootstrap/readers/ExchangeCsvReader.java
@@ -0,0 +1,58 @@
+package rs.edu.raf.StockService.bootstrap.readers;
+
+import rs.edu.raf.StockService.data.entities.Exchange;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
+
+public class ExchangeCsvReader {
+    private static final String FILE_PATH = "StockService/src/main/resources/csvs/exchanges.csv";
+
+    public static List<Exchange> loadExchangeCsv() {
+        List<Exchange> data = new ArrayList<>();
+        String line;
+        String cvsSplitBy = ",";
+        long i = 0;
+        boolean isFirst = true;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(ExchangeCsvReader.FILE_PATH))) {
+            while ((line = reader.readLine()) != null) {
+                String[] row = line.split(cvsSplitBy);
+
+                TimeZone timeZone = TimeZone.getTimeZone(row[5]);
+                ZonedDateTime currentTime = ZonedDateTime.now(timeZone.toZoneId());
+                ZonedDateTime currentTimeInUTC = ZonedDateTime.now(ZoneId.of("UTC"));
+                Integer timeZoneDiff = currentTime.getHour() - currentTimeInUTC.getHour();
+
+                Exchange exchange = new Exchange(
+                        row[0],
+                        row[1],
+                        row[2],
+                        row[3],
+                        row[4],
+                        timeZoneDiff
+                );
+
+                if (isFirst) {
+                    isFirst = false;
+                    continue;
+                }
+
+                data.add(exchange);
+                exchange.setId(i);
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+
+}
