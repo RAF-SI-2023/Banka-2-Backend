140f64b58d804132c12ddacbd312d61a3214e67b
SrdjanSimonovic9220 <ssimonovic9220rn@raf.rs>
2024-03-13 16:41:30 +0100

    Uncomment in StockServiceApplicationTests class.

--- StockService/src/test/java/rs/edu/raf/StockService/StockServiceApplicationTests.java
+++ StockService/src/test/java/rs/edu/raf/StockService/StockServiceApplicationTests.java
@@ -1,13 +1,13 @@
-//package rs.edu.raf.StockService;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class StockServiceApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package rs.edu.raf.StockService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
