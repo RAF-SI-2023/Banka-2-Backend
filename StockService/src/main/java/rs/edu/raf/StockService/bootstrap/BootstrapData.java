95cc7d32eafc714162118423e8c1b3c75c15fbd7
PetarRandjelovic <prandjelovic11221rn@raf.rs>
2024-03-17 14:52:14 +0100

    Uradjen Entitet option, takodje dto, mapper neke od njegovih ruta i testovi.

--- StockService/src/main/java/rs/edu/raf/StockService/bootstrap/BootstrapData.java
+++ StockService/src/main/java/rs/edu/raf/StockService/bootstrap/BootstrapData.java
@@ -8,11 +8,18 @@ import org.springframework.stereotype.Component;
 import rs.edu.raf.StockService.bootstrap.readers.CurrencyCsvReader;
 import rs.edu.raf.StockService.data.entities.Currency;
 import rs.edu.raf.StockService.data.entities.CurrencyInflation;
+import rs.edu.raf.StockService.data.entities.Option;
+import rs.edu.raf.StockService.data.enums.OptionType;
 import rs.edu.raf.StockService.repositories.CurrencyInflationRepository;
 import rs.edu.raf.StockService.repositories.CurrencyRepository;
+import rs.edu.raf.StockService.repositories.OptionRepository;
 import rs.edu.raf.StockService.services.impl.InMemoryCurrencyInflationServiceImpl;
 import rs.edu.raf.StockService.services.impl.InMemoryCurrencyServiceImpl;
+import rs.edu.raf.StockService.services.impl.OptionServiceImpl;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.List;
 
 @Component
@@ -29,15 +36,21 @@ public class BootstrapData implements CommandLineRunner {
     private final InMemoryCurrencyInflationServiceImpl currencyInflationService;
     private final ResourceLoader resourceLoader;
 
+    private final OptionServiceImpl optionServiceImpl;
+
+    private final OptionRepository optionRepository;
+
     public BootstrapData(CurrencyRepository currencyRepository,
                          CurrencyInflationRepository currencyInflationRepository,
                          InMemoryCurrencyServiceImpl currencyService,
-                         InMemoryCurrencyInflationServiceImpl currencyInflationService, ResourceLoader resourceLoader) {
+                         InMemoryCurrencyInflationServiceImpl currencyInflationService, ResourceLoader resourceLoader, OptionServiceImpl optionServiceImpl, OptionRepository optionRepository) {
         this.currencyRepository = currencyRepository;
         this.currencyInflationRepository = currencyInflationRepository;
         this.currencyService = currencyService;
         this.currencyInflationService = currencyInflationService;
         this.resourceLoader = resourceLoader;
+        this.optionServiceImpl = optionServiceImpl;
+        this.optionRepository = optionRepository;
     }
 
     /**
@@ -54,6 +67,30 @@ public class BootstrapData implements CommandLineRunner {
          currencyRepository.saveAll(currencyList);    */
         currencyService.setCurrencyList(currencyList);
         currencyInflationService.setCurrencyInflationList(currencyInflationList);
+
+        LocalDate currentDate = LocalDate.now();
+        LocalDateTime localDateTime = currentDate.atStartOfDay();
+        long milliseconds = localDateTime.toInstant(ZoneOffset.UTC).toEpochMilli();
+        for (int i = 1; i <=3 ; i++) {
+            Option option = new Option();
+            option.setStockListing("STOCK" + i);
+            option.setOptionType(OptionType.PUT);
+            option.setStrikePrice("100" + i);
+            option.setImpliedVolatility("420" + i);
+            option.setOpenInterest(120+"i");
+            option.setSettlementDate(milliseconds);
+
+            optionRepository.save(option);
+
+        }
+        Option option = new Option();
+        option.setStockListing("STOCK" + 1);
+        option.setOptionType(OptionType.PUT);
+        option.setStrikePrice("100" + 1);
+        option.setImpliedVolatility("420" + 1);
+        option.setOpenInterest(120+"i");
+        option.setSettlementDate(milliseconds);
+        optionRepository.save(option);
         logger.info("DATA LOADING FINISHED...");
     }
 }
--- StockService/src/main/java/rs/edu/raf/StockService/configuration/SpringSecurityConfig.java
+++ StockService/src/main/java/rs/edu/raf/StockService/configuration/SpringSecurityConfig.java
@@ -1,31 +1,31 @@
-//package rs.edu.raf.StockService.configuration;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SpringSecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//        http
-//                .cors(Customizer.withDefaults())
-//                .csrf(Customizer.withDefaults())
-//                .authorizeHttpRequests(
-//                        requests -> requests
-//                                .anyRequest().permitAll()
-//                )
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .httpBasic(Customizer.withDefaults());
-//
-//        return http.build();
-//    }
-//
-//}
+package rs.edu.raf.StockService.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(
+                        requests -> requests
+                                .anyRequest().permitAll()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+}
