7a73cf550c74aeb3be8189e1629216b7268530f9
GajicM <mgajic9821rn@raf.rs>
2024-03-18 12:58:14 +0100

    DockerCompose za stock, kao i redis za currency, obavezno proveriti jedno 16 puta

--- StockService/src/main/java/rs/edu/raf/StockService/bootstrap/BootstrapData.java
+++ StockService/src/main/java/rs/edu/raf/StockService/bootstrap/BootstrapData.java
@@ -4,16 +4,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import rs.edu.raf.StockService.bootstrap.readers.CurrencyCsvReader;
 import rs.edu.raf.StockService.data.entities.Currency;
 import rs.edu.raf.StockService.data.entities.CurrencyInflation;
 import rs.edu.raf.StockService.repositories.CurrencyInflationRepository;
 import rs.edu.raf.StockService.repositories.CurrencyRepository;
+import rs.edu.raf.StockService.services.CurrencyInflationService;
+import rs.edu.raf.StockService.services.CurrencyService;
+import rs.edu.raf.StockService.services.impl.CurrencyInflationServiceImpl;
+import rs.edu.raf.StockService.services.impl.CurrencyServiceImpl;
 import rs.edu.raf.StockService.services.impl.InMemoryCurrencyInflationServiceImpl;
 import rs.edu.raf.StockService.services.impl.InMemoryCurrencyServiceImpl;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class BootstrapData implements CommandLineRunner {
@@ -24,20 +31,24 @@ public class BootstrapData implements CommandLineRunner {
 
     private final CurrencyInflationRepository currencyInflationRepository;
 
-    private final InMemoryCurrencyServiceImpl currencyService;
-
-    private final InMemoryCurrencyInflationServiceImpl currencyInflationService;
     private final ResourceLoader resourceLoader;
+    /**
+     * za redis, ne koristiti ovde najverovatnije
+     */
+    private final RedisTemplate<String, Currency> redisCurrency;
+    private final RedisTemplate<String, CurrencyInflation> redisCurrencyInflation;
+
 
     public BootstrapData(CurrencyRepository currencyRepository,
                          CurrencyInflationRepository currencyInflationRepository,
-                         InMemoryCurrencyServiceImpl currencyService,
-                         InMemoryCurrencyInflationServiceImpl currencyInflationService, ResourceLoader resourceLoader) {
+                         ResourceLoader resourceLoader,
+                         RedisTemplate<String, Currency> redisTemplate,
+                         RedisTemplate<String, CurrencyInflation> redisCurrencyInflation) {
         this.currencyRepository = currencyRepository;
         this.currencyInflationRepository = currencyInflationRepository;
-        this.currencyService = currencyService;
-        this.currencyInflationService = currencyInflationService;
         this.resourceLoader = resourceLoader;
+        this.redisCurrency = redisTemplate;
+        this.redisCurrencyInflation = redisCurrencyInflation;
     }
 
     /**
@@ -47,13 +58,35 @@ public class BootstrapData implements CommandLineRunner {
     public void run(String... args) {
         logger.info("DATA LOADING IN PROGRESS...");
         CurrencyCsvReader currencyCsvReader = new CurrencyCsvReader(resourceLoader);
-        List<Currency> currencyList = currencyCsvReader.loadCurrencyData();
-        List<CurrencyInflation> currencyInflationList = currencyCsvReader.pullCurrencyInflationData(currencyList);
-        /*
-         * otkomentarisati ako cemo koristiti bazu, a ne in memory listu , slicno i za currencyInflation
-         currencyRepository.saveAll(currencyList);    */
-        currencyService.setCurrencyList(currencyList);
-        currencyInflationService.setCurrencyInflationList(currencyInflationList);
+        List<Currency> currencyList;
+        List<CurrencyInflation> currencyInflationList;
+         /* REDIS
+          cacheCurrencies(currencyList);
+        cacheCurrencyInflation(currencyInflationList);*/
+        if (currencyInflationRepository.count() == 0 || currencyRepository.count() == 0) {
+            currencyList = currencyCsvReader.loadCurrencyData();
+            currencyList = currencyRepository.saveAll(currencyList);
+            currencyInflationList = currencyCsvReader.pullCurrencyInflationData(currencyList);
+            currencyInflationRepository.saveAll(currencyInflationList);
+        }
+        /*za in memory
+        //  currencyService.setCurrencyList(currencyList);
+        //  currencyInflationService.setCurrencyInflationList(currencyInflationList);
+        */
+
         logger.info("DATA LOADING FINISHED...");
     }
+
+    //**Iskljucivo za redis, da se sve odmah kesira, nema nekog smisla.*/
+ /*   private void cacheCurrencies(List<Currency> currencies) {
+        for (Currency currency : currencies) {
+            redisCurrency.opsForValue().set("currency: code=" + currency.getCurrencyCode() + " id=" + currency.getId(), currency);
+        }
+    }
+
+    private void cacheCurrencyInflation(List<CurrencyInflation> currencyInflations) {
+        for (CurrencyInflation currencyInflation : currencyInflations) {
+            redisCurrencyInflation.opsForValue().set("currencyInflation:id=" + (currencyInflation.getId()) + " currencyId=" + currencyInflation.getCurrencyId(), currencyInflation);
+        }
+    }*/
 }
