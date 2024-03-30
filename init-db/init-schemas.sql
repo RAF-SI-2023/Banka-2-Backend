54f64c2abf857a4b6aac84e8287eed4491150deb
luka <lpavlovic032@gmail.com>
2024-03-30 13:56:09 +0100

    scripts for database

---
+++ init-db/init-schemas.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA IF NOT EXISTS iam_service_schema;
+
+CREATE SCHEMA IF NOT EXISTS bank_service_schema;
+
+CREATE SCHEMA IF NOT EXISTS notification_service_schema;
+
+CREATE SCHEMA IF NOT EXISTS stock_service_schema;
+
+
+
