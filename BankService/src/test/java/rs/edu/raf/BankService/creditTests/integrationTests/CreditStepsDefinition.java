3294d4abd4bf124b1ea140c752a5d048bfc5de1c
GajicM <mgajic9821rn@raf.rs>
2024-04-05 20:36:54 +0200

    integracioni testovi za kredit i findAccountsByEmail

--- BankService/src/test/java/rs/edu/raf/BankService/creditTests/integrationTests/CreditStepsDefinition.java
+++ BankService/src/test/java/rs/edu/raf/BankService/creditTests/integrationTests/CreditStepsDefinition.java
@@ -3,38 +3,31 @@ package rs.edu.raf.BankService.creditTests.integrationTests;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.web.servlet.MockMvc;
 import rs.edu.raf.BankService.controller.AccountController;
-import rs.edu.raf.BankService.controller.CreditController;
 import rs.edu.raf.BankService.data.dto.CreditDto;
+import rs.edu.raf.BankService.data.dto.CreditRequestDto;
+import rs.edu.raf.BankService.data.enums.CreditRequestStatus;
+import rs.edu.raf.BankService.data.enums.CreditType;
 import rs.edu.raf.BankService.service.CreditService;
 
 import java.util.List;
+import java.util.Random;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.junit.Assert.*;
 
 public class CreditStepsDefinition {
     private CreditService creditService;
-    private AccountController accountController;
-    @MockBean
-    SecurityContext securityContextHolder;
-    @MockBean
-    MockMvc mockMvc;
 
-    public CreditStepsDefinition(CreditService creditService, AccountController accountController) {
+
+    public CreditStepsDefinition(CreditService creditService) {
         this.creditService = creditService;
-        this.accountController = accountController;
     }
 
     String accountNumber;
     List<CreditDto> credits;
-
+    CreditRequestDto creditRequestDto;
+    Object response;
 
     @Given("user has an account with account number {string}")
     public void userHasAnAccount(String string) {
@@ -44,13 +37,78 @@ public class CreditStepsDefinition {
 
     @When("user checks his credits\\/loans")
     @WithMockUser(username = "admin", authorities = {"ADMIN", "USER"}, roles = "ADMIN")
-    public void userLogsInAndChecksTheCredits() throws Exception {
+    public void userChecksHisCreditsLoans() throws Exception {
         credits = creditService.getCreditsByAccountNumber(accountNumber);
     }
 
     @Then("he should see his own credits")
     public void heShouldSeeHisOwnCredits() {
-        assertEquals(3, credits.size());
+        assertNotNull(credits);
+    }
+
+    @Then("he should see a confirmation message")
+    public void heShouldSeeAConfirmationMessage() {
+        if (response instanceof CreditRequestDto)
+            assertNotNull(((CreditRequestDto) response).getId());
+        else
+            fail();
 
     }
+
+    @When("user creates a credit request on his account number {string}")
+    public void userCreatesACreditRequestOnHisAccountNumber(String arg0) {
+        creditRequestDto = new CreditRequestDto();
+        creditRequestDto.setAccountNumber(arg0);
+        creditRequestDto.setCreditAmount(36000.0);
+        creditRequestDto.setCreditPurpose("STAMBENI");
+        creditRequestDto.setCurrency("EUR");
+        creditRequestDto.setEducationLevel("srednja");
+        creditRequestDto.setEmploymentPeriod(5L);
+        creditRequestDto.setHousingStatus("iznajmljen");
+        creditRequestDto.setMaritalStatus("neozenjen");
+        creditRequestDto.setMobileNumber("0655555555");
+        creditRequestDto.setMonthlySalary(1000L);
+        creditRequestDto.setPermanentEmployment(true);
+        creditRequestDto.setOwnCar(false);
+        creditRequestDto.setBranch("Novi Sad");
+        creditRequestDto.setMaturity(12L);
+        creditRequestDto.setCreditType(CreditType.STAMBENI);
+        creditRequestDto.setNote("pls daj kredit");
+        creditRequestDto.setPaymentPeriodMonths(new Random().nextLong(12, 36));
+        response = creditService.createCreditRequest(creditRequestDto);
+    }
+
+    @When("employee checks credit requests")
+    public void employeeChecksCreditRequests() {
+        response = creditService.getAllCreditRequests();
+    }
+
+    @Then("he should see all credit requests")
+    public void heShouldSeeAllCreditRequests() {
+        assertNotNull(response);
+        assertTrue(response instanceof List);
+        assertFalse(((List<?>) response).isEmpty());
+    }
+
+
+    @Given("employee sees a credit request")
+    public void employeeSeesACreditRequest() {
+        creditRequestDto = creditService.getAllCreditRequests().stream().filter(creditRequestDto -> creditRequestDto.getStatus().equals(CreditRequestStatus.PENDING)).findFirst().orElse(null);
+        if (creditRequestDto == null) {
+            fail("No pending credit requests");
+        }
+    }
+
+    @When("employee approves the credit request")
+    public void employeeApprovesTheCreditRequest() {
+        response = creditService.approveCreditRequest(creditRequestDto.getId());
+    }
+
+    @Then("the credit request should be approved and credit created")
+    public void theCreditRequestShouldBeApprovedAndCreditCreated() {
+        assertNotNull(response);
+        assertTrue(response instanceof CreditDto);
+        assertEquals(creditRequestDto.getCreditAmount(), ((CreditDto) response).getCreditAmount(), 0.1);
+    }
+
 }
