a889f1dccaeb30efc9e1276be2cdae63b596d853
GajicM <mgajic9821rn@raf.rs>
2024-04-09 17:21:14 +0200

    menjanje putanja za testove

--- BankService/src/test/java/rs/edu/raf/BankService/creditTests/e2e/CreditControllerTests.java
+++ BankService/src/test/java/rs/edu/raf/BankService/creditTests/e2e/CreditControllerTests.java
@@ -1,19 +1,15 @@
 package rs.edu.raf.BankService.creditTests.e2e;
 
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.core.options.Constants;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-import org.junit.runner.RunWith;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-
-@CucumberOptions(features = "src/test/java/resources/e2e", glue = "rs.edu.raf.BankService.creditTests.e2e", tags = "not @Ignore")
-@RunWith(Cucumber.class)
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "rs.edu.raf.BankService.creditTests.e2e")
-@SelectClasspathResource("e2e")
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features/e2e/creditController")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "rs.edu.raf.BankService.creditTests.e2e")
 public class CreditControllerTests {
 }
--- BankService/src/test/java/rs/edu/raf/BankService/creditTests/integrationTests/CreditIntegrationTests.java
+++ BankService/src/test/java/rs/edu/raf/BankService/creditTests/integrationTests/CreditIntegrationTests.java
@@ -1,12 +1,17 @@
 package rs.edu.raf.BankService.creditTests.integrationTests;
 
+import io.cucumber.core.options.Constants;
 import io.cucumber.junit.Cucumber;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/resources/integration", glue = "rs.edu.raf.BankService.creditTests.integrationTests", tags = "not @Ignore")
-@RunWith(Cucumber.class)
-@SelectClasspathResource("integration")
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features/integration/credit-integration")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "rs.edu.raf.BankService.creditTests.integrationTests")
 public class CreditIntegrationTests {
 }
