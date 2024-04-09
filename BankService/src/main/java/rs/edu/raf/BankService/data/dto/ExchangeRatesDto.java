ee6a04494e28135203f18bbf83dd536093383be9
GajicM <mgajic9821rn@raf.rs>
2024-04-09 18:14:48 +0200

    unit testovi za menjacnicu

--- BankService/src/main/java/rs/edu/raf/BankService/data/dto/ExchangeRatesDto.java
+++ BankService/src/main/java/rs/edu/raf/BankService/data/dto/ExchangeRatesDto.java
@@ -2,9 +2,11 @@ package rs.edu.raf.BankService.data.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ExchangeRatesDto {
     private Long id;
     private long timeLastUpdated;
--- BankService/src/main/java/rs/edu/raf/BankService/data/dto/ExchangeRequestDto.java
+++ BankService/src/main/java/rs/edu/raf/BankService/data/dto/ExchangeRequestDto.java
@@ -2,9 +2,11 @@ package rs.edu.raf.BankService.data.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ExchangeRequestDto {
     private String fromAccount;
     private String fromCurrency;
