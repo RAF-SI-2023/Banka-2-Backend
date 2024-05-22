16e83bd4349ee50bf39d770514a88bb878347d89
GajicM <92932048+GajicM@users.noreply.github.com>
2024-05-22 13:55:50 +0200

    test (#280)
    * uradjeni buy/sell trading jobs
    
    * otc
    
    * trading simulation change
    
    * change
    
    * ispravljen contract i taj workflow
    
    * popravljen i create order
    
    * bugfix-es
    
    * bugfix
    
    * test
    
    * test

--- BankService/src/main/java/rs/edu/raf/BankService/data/dto/AgentDto.java
+++ BankService/src/main/java/rs/edu/raf/BankService/data/dto/AgentDto.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class AgentDto {
-
     private Double userLimit;
     private Double leftOfLimit;
     private Boolean orderApprovalRequired;
