64687cdfb73be0f57fe71f2d78413238266b75c2
Petar Randjelovic <92932182+PetarRandjelovic@users.noreply.github.com>
2024-06-09 13:48:09 +0200

    Napravljeni unit testove za ActionAgentProfitService, BankProfitServiceImpl i BankTransferTransactionDetailsServiceImpl (#358)
    * Napravljena entitet BankTransferTransactionDetail
    
    * Napravljena entitet BankTransferTransactionDetail
    
    * Napravljena entitet BankTransferTransactionDetail
    
    * Sada radi maven lifecycle test
    
    * Sada radi maven lifecycle test
    
    * Sada radi maven lifecycle test
    
    * Izmenjen entinet Card dodat block i metode vezane za to
    
    * Izmenjen entinet Card dodat block i metode vezane za to
    
    * Bootstrap ima karticu ponovo
    
    * Bootstrap ima karticu ponovo
    
    * Fix
    
    * Profit za individualne agente, samo za akcije i Profit na nivou celokupne banke je uradjeno
    
    * Dodat getAllApprovedContracts i getAllRejectedContracts rute za Contract
    
    * Dodat ruta za ukupan profit od agenata
    
    * Dodat ruta za ukupan profit od agenata
    
    * Dodat ruta za ukupan profit od agenata
    
    * Dodat ruta za ukupan profit od agenata
    
    * Popravka za currencyExchange test
    
    * Napravljeni unit testove za ActionAgentProfitService, BankProfitServiceImpl i BankTransferTransactionDetailsServiceImpl

---
+++ bank-service/src/test/java/rs/edu/raf/BankService/unit/profitTransactionsTests/BankProfitServiceImplUnitTests.java
@@ -0,0 +1,81 @@
+package rs.edu.raf.BankService.unit.profitTransactionsTests;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import rs.edu.raf.BankService.mapper.BankProfitMapper;
+import rs.edu.raf.BankService.repository.ActionAgentProfitRepository;
+import rs.edu.raf.BankService.repository.BankProfitRepository;
+import rs.edu.raf.BankService.repository.BankTransferTransactionDetailsRepository;
+import rs.edu.raf.BankService.service.impl.BankProfitServiceImpl;
+import org.junit.jupiter.api.Test;
+import rs.edu.raf.BankService.data.dto.BankProfitDto;
+import rs.edu.raf.BankService.data.entities.exchangeCurrency.BankTransferTransactionDetails;
+import rs.edu.raf.BankService.data.entities.profit.ActionAgentProfit;
+import rs.edu.raf.BankService.data.entities.profit.BankProfit;
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+public class BankProfitServiceImplUnitTests {
+    @Mock
+    private  ActionAgentProfitRepository actionAgentProfitRepository;
+    @Mock
+    private  BankTransferTransactionDetailsRepository bankTransferTransactionDetailsRepository;
+    @Mock
+    private  BankProfitRepository bankProfitRepository;
+    @Mock
+    private  BankProfitMapper bankProfitMapper;
+
+    @InjectMocks
+    private BankProfitServiceImpl bankProfitService;
+
+
+    @Test
+    void getTotalProfit_shouldCalculateTotalProfitCorrectly() {
+        // Arrange
+        ActionAgentProfit actionAgentProfit = new ActionAgentProfit();
+        actionAgentProfit.setProfit(100.0);
+
+        BankTransferTransactionDetails bankTransferTransactionDetails = new BankTransferTransactionDetails();
+        bankTransferTransactionDetails.setTotalProfit(200.0);
+
+        BankProfit bankProfit = new BankProfit();
+        BankProfitDto bankProfitDto = new BankProfitDto();
+
+        when(actionAgentProfitRepository.findAll()).thenReturn(Collections.singletonList(actionAgentProfit));
+        when(bankTransferTransactionDetailsRepository.findAll()).thenReturn(Collections.singletonList(bankTransferTransactionDetails));
+        when(bankProfitRepository.findById(1L)).thenReturn(Optional.of(bankProfit));
+        when(bankProfitMapper.bankProfitToBankProfitDto(any(BankProfit.class))).thenReturn(bankProfitDto);
+
+        // Act
+        BankProfitDto result = bankProfitService.getTotalProfit();
+
+        // Assert
+        assertEquals(bankProfitDto, result);
+        verify(bankProfitRepository, times(1)).save(bankProfit);
+        assertEquals(300.0, bankProfit.getProfit());
+    }
+
+    @Test
+    void getTotalProfit_shouldHandleEmptyProfit() {
+        // Arrange
+        when(actionAgentProfitRepository.findAll()).thenReturn(Collections.emptyList());
+        when(bankTransferTransactionDetailsRepository.findAll()).thenReturn(Collections.emptyList());
+        when(bankProfitRepository.findById(1L)).thenReturn(Optional.empty());
+        when(bankProfitMapper.bankProfitToBankProfitDto(any(BankProfit.class))).thenReturn(new BankProfitDto());
+
+        // Act
+        BankProfitDto result = bankProfitService.getTotalProfit();
+
+        // Assert
+        assertEquals(0.0, result.getProfit());
+        verify(bankProfitRepository, times(1)).save(any(BankProfit.class));
+    }
+}
--- stock-service/src/test/resources/features/e2e/optioncontroller/OptionController.feature
+++ stock-service/src/test/resources/features/e2e/optioncontroller/OptionController.feature
@@ -1,5 +1,5 @@
 Feature: OptionController API
 
   Scenario: Retrieve options by stock listing
-    When the client wants to requests options for root "E"
+    When the client wants to requests options for root "Z"
     Then option should return value list by stock listing
