80c7374da5e374927f0fdbb76ab355a601a7aabf
Petar Randjelovic <92932182+PetarRandjelovic@users.noreply.github.com>
2024-06-29 20:55:30 +0200

    contract e2e test (#384)

---
+++ otc-service/src/test/java/rs/edu/raf/OTCService/e2e/buyingAndSellingOrders/BuyingAndSellingOrdersTestsConfigTest.java
@@ -0,0 +1,11 @@
+package rs.edu.raf.OTCService.e2e.buyingAndSellingOrders;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest
+@AutoConfigureMockMvc
+public class BuyingAndSellingOrdersTestsConfigTest {
+}
